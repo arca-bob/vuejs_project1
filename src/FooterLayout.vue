014354ac931e35cabbb81a3f7d6f3541cc91e4f4
arcabob <159237376+arca-bob@users.noreply.github.com>
2025-03-25 23:58:59 +0100

    Add files via upload

---
+++ src/FooterLayout.vue
@@ -0,0 +1,16 @@
+<template>
+        <footer>
+    <p>&copy; 2025 My Website</p>
+</footer>
+</template>
+
+<style>
+footer {
+    margin-top: 5%;
+    background-color: lightgray;
+    justify-content: center;
+    width: 100%;
+    display: flex;  
+}
+
+</style>
---
+++ src/HeaderLayout.vue
@@ -0,0 +1,69 @@
+<template>
+
+    <header>
+        <RouterLink to="/"><img src="../logo.png" alt="home"width="45" id = "headericon"></RouterLink>
+        <RouterLink to="/login"><img src="../user.svg" alt="user"width="30"></RouterLink> 
+        •
+        <RouterLink to="/cart"><img src="../shopping-cart.svg" alt="panier" width="30"></RouterLink> 
+        
+        <form action="recherche.php" method="get" id="headersearch">
+            <div class="headerSearch">
+                <input type="text" placeholder="Rechercher un produit..."id="recherche">
+            </div>
+            <img src="../search.svg" width="25px" alt="recherche" id="recherche2">
+        </form>
+        </header>
+</template>
+
+<style>
+
+
+header {
+    position: sticky;
+    width: 80%;
+    padding-top: 20px;
+    margin-top: 20px;
+    margin-left: 10%;
+    background-color: lightgrey;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    display: flex;
+    top: 0;
+    text-decoration: none;
+    border-radius: 100px;
+
+}
+#headericon {
+    margin-right: 90px;
+    padding-bottom: 20px;
+}
+#headersearch {
+    height: 50%;
+    width: 20%;
+    margin-bottom: 15px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: white;
+    border-radius: 50px;
+
+}
+
+
+
+
+#recherche {
+    height: 100%;
+    width: 84%;
+    margin: 10%;
+    border: none;
+    background-color: white;
+    
+}
+#recherche2 {
+    height: 1%;
+    margin: 5%;
+}
+
+
+</style>
